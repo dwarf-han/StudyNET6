b3da449923695776880ab9fe29da35cb156733cd
dwarf.han <dwarf.han@gmail.com>
2022-03-21 00:32:43 +0900

    Documentation Sample App

---
+++ StudyApp/Model/Book.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyApp.Model
+{
+    /// <summary>
+    /// 책 모델
+    /// </summary>
+    public class Book
+    {
+        /// <summary>
+        /// 생성 일자 
+        /// </summary>
+        public DateTime CreatedTime { get; set; }
+
+
+        /// <summary>
+        /// 책 종류
+        /// </summary>
+        public BookType Type { get; set; }
+
+        /// <summary>
+        /// 책 이름 
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 책을 생성합니다.
+        /// </summary>
+        /// <param name="createdTime">생성 일자</param>
+        /// <param name="type">종류</param>
+        /// <param name="title">이름</param>
+        /// <code>
+        /// var b = new Book(DateTime.Now, BookType.Game, "Diablo II");
+        /// </code>
+        public Book(DateTime createdTime, BookType type, string title)
+        {
+            CreatedTime = createdTime;
+            Type = type;
+            Title = title;
+        }
+
+        /// <summary>
+        /// 책 정보 출력
+        /// </summary>
+        /// <returns>책 정보를 출력</returns>
+        public override string ToString()
+        {
+            return $"{CreatedTime} {Type}";
+        }
+    }
+
+    /// <summary>
+    /// 북 종류
+    /// </summary>
+    public enum BookType
+    {
+        /// <summary>
+        /// 게임
+        /// </summary>
+        Game,
+
+        /// <summary>
+        /// 역사
+        /// </summary>
+        History
+    }
+}
